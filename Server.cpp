6931768dbc1cd866c7c7424bef8df9dc7275691f
elayben <elaybencp3@gmail.com>
2024-08-13 22:14:17 +0300

    add multithreaded

--- Server.cpp
+++ Server.cpp
@@ -3,7 +3,38 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <cstring>
-using namespace std;
+#include <thread>
+
+// Function to handle communication with the client
+void handle_client(int client_sock) {
+    char buffer[4096];
+    int expected_data_len = sizeof(buffer);
+
+    while (true) {
+        // Initialize the buffer with null characters
+        memset(buffer, 0, expected_data_len);
+
+        int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+        if (read_bytes == 0) {
+            std::cout << "Connection closed by client" << std::endl;
+            break;
+        } else if (read_bytes < 0) {
+            perror("Error receiving data");
+            break;
+        } else {
+            std::cout << "Received from client: " << buffer << std::endl;
+
+            int sent_bytes = send(client_sock, buffer, read_bytes, 0);
+            if (sent_bytes < 0) {
+                perror("Error sending to client");
+                break;
+            }
+        }
+    }
+
+    close(client_sock);  // Close the client socket when done
+}
+
 
 int main() {
     const int server_port = 5555;
@@ -11,7 +42,7 @@ int main() {
     // Create socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        perror("error creating socket");
+        perror("Error creating socket");
         return 1;
     }
 
@@ -24,49 +55,36 @@ int main() {
 
     // Bind the socket
     if (bind(sock, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
-        perror("error binding socket");
+        perror("Error binding socket");
         close(sock);
         return 1;
     }
 
     // Listen to the socket
     if (listen(sock, 5) < 0) {
-        perror("error listening to socket");
+        perror("Error listening to socket");
         close(sock);
         return 1;
     }
 
-    // Accept client connection
-    struct sockaddr_in client_sin;
-    unsigned int addr_len = sizeof(client_sin);
-    int client_sock = accept(sock, (struct sockaddr*)&client_sin, &addr_len);
-    if (client_sock < 0) {
-        perror("error accepting client");
-        close(sock);
-        return 1;
+    std::cout << "Server is listening on port " << server_port << std::endl;
+
+    while (true) {
+        // Accept client connection
+        struct sockaddr_in client_sin;
+        unsigned int addr_len = sizeof(client_sin);
+        int client_sock = accept(sock, (struct sockaddr*)&client_sin, &addr_len);
+        if (client_sock < 0) {
+            perror("Error accepting client");
+            close(sock);
+            return 1;
+        }
+
+        // Create a new thread to handle the client
+        std::thread(handle_client, client_sock).detach();
     }
 
-    // Receive data from client
-    char buffer[4096];
-    int expected_data_len = sizeof(buffer);
-    int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-    if (read_bytes == 0) {
-        cout << "connection is closed by client" << endl;
-    } else if (read_bytes < 0) {
-        perror("error receiving data");
-    } else {
-        cout << "Received from client: " << buffer << endl;
-    }
-
-    // Send data back to client (echo)
-    int sent_bytes = send(client_sock, buffer, read_bytes, 0);
-    if (sent_bytes < 0) {
-        perror("error sending to client");
-    }
-
-    // Close sockets
-    close(client_sock);
-    close(sock);
+    close(sock);  // Close the server socket when done
 
     return 0;
 }
